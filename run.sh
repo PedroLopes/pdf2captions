494afa9e6f16509ea547ba0dd2e1746726724ad4
Pedro Lopes <plopesresearch@gmail.com>
2015-08-18 14:15:52 +0200

    added a time counter and nicer output

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-echo "$*"
+#!/bin/bash
+echo Generating captions from PDF file "$*"
+START=$(date +%s)
 pdf2txt.py "$*" > "$*".txt
 python parseCaptionsFormText.py "$*".txt > "$*"_captions.txt
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "Captions generated, see "$*"_captions.txt, took $DIFF seconds"
+
